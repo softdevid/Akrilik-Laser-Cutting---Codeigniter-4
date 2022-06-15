0d360ac2a220b9f123949bf63c1dd58f9a910cdc
Ardianto <ardianto8306@gmail.com>
2022-06-15 16:21:55 +0700

    ser4

--- app/Views/home/services.php
+++ app/Views/home/services.php
@@ -23,7 +23,7 @@
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     <div class="col">
                         <div class="card h-100" style="border: none;">
-                            <img src="..." class="card-img-top" alt="...">
+                            <img src="/img/services/web.jpg" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Pembuatan Website</h5>
                                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -101,7 +101,7 @@
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     <div class="col">
                         <div class="card h-100" style="border: none;">
-                            <img src="..." class="card-img-top" alt="...">
+                            <img src="/img/services/web.jpg" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Pembuatan Website</h5>
                                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
