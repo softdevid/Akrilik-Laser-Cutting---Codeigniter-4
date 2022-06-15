c109227c63b605b1eda3b1bc562f53435378def8
zionexeonch <zionexeon@gmail.com>
2022-06-16 06:01:42 +0700

    mengubah isi code

--- app/Views/home/portfolio.php
+++ app/Views/home/portfolio.php
@@ -4,16 +4,16 @@
     <div class="background-portfolio">
         <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
             <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home"
-                    type="button" role="tab" aria-controls="pills-home" aria-selected="true">All</button>
+                <button class="nav-link active" id="pills-all-tab" data-bs-toggle="pill" data-bs-target="#pills-all"
+                    type="button" role="tab" aria-controls="pills-all" aria-selected="true">All</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile"
-                    type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Huruf Timbul</button>
+                <button class="nav-link" id="pills-timbul-tab" data-bs-toggle="pill" data-bs-target="#pills-timbul"
+                    type="button" role="tab" aria-controls="pills-timbul" aria-selected="false">Huruf Timbul</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact"
-                    type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Neonbox</button>
+                <button class="nav-link" id="pills-neonbox-tab" data-bs-toggle="pill" data-bs-target="#pills-neonbox"
+                    type="button" role="tab" aria-controls="pills-neonbox" aria-selected="false">Neonbox</button>
             </li>
             <li class="nav-item" role="presentation">
                 <button class="nav-link" id="pills-reklame-tab" data-bs-toggle="pill" data-bs-target="#pills-reklame"
@@ -31,31 +31,94 @@
             </li>
         </ul>
         <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab"
+            <div class="tab-pane fade show active" id="pills-all" role="tabpanel" aria-labelledby="pills-all-tab"
                 tabindex="0">
-                <div class="card bg-dark text-white">
-                    <img src="..." class="card-img" alt="...">
-                    <div class="card-img-overlay">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                        <p class="card-text">Last updated 3 mins ago</p>
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card bg-black overlay card-hover border-0">
+                            <a href="#" class="text-white">
+                                <img src="/img/portfolio.png" class="card-img" alt="Portfolio">
+                                <div class="overlay-content card-img-overlay text-bottom">
+                                    <h5>Website Development</h5>
+                                    <span>Digital Creative, Website Development</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card bg-black overlay card-hover border-0">
+                            <a href="#" class="text-white">
+                                <img src="/img/kanopi.jpg" class="card-img" alt="Kanopi">
+                                <div class="overlay-content card-img-overlay text-bottom">
+                                    <h5>Kanopi</h5>
+                                    <span>Pekerjaan Besi dan Aluminium</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card bg-black overlay card-hover border-0">
+                            <a href="#" class="text-white">
+                                <img src="/img/pagarbesi.jpg" class="card-img" alt="Pagar Besi">
+                                <div class="overlay-content card-img-overlay text-bottom">
+                                    <h5>Pagar Besi</h5>
+                                    <span>Pekerjaan Besi dan Aluminium</span>
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab"
+            <div class="tab-pane fade" id="pills-timbul" role="tabpanel" aria-labelledby="pills-timbul-tab"
                 tabindex="0">
                 ...</div>
-            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab"
+            <div class="tab-pane fade" id="pills-neonbox" role="tabpanel" aria-labelledby="pills-neonbox-tab"
                 tabindex="0">
                 ...</div>
             <div class="tab-pane fade" id="pills-reklame" role="tabpanel" aria-labelledby="pills-reklame-tab"
                 tabindex="0">
                 ...</div>
             <div class="tab-pane fade" id="pills-besi" role="tabpanel" aria-labelledby="pills-besi-tab" tabindex="0">
-                ...</div>
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card bg-black overlay card-hover border-0">
+                            <a href="#" class="text-white">
+                                <img src="/img/kanopi.jpg" class="card-img" alt="Kanopi">
+                                <div class="overlay-content card-img-overlay text-bottom">
+                                    <h5>Kanopi</h5>
+                                    <span>Pekerjaan Besi dan Aluminium</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card bg-black overlay card-hover border-0">
+                            <a href="#" class="text-white">
+                                <img src="/img/pagarbesi.jpg" class="card-img" alt="Pagar Besi">
+                                <div class="overlay-content card-img-overlay text-bottom">
+                                    <h5>Pagar Besi</h5>
+                                    <span>Pekerjaan Besi dan Aluminium</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="tab-pane fade" id="pills-web" role="tabpanel" aria-labelledby="pills-web-tab" tabindex="0">
-                ...</div>
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card bg-black overlay card-hover border-0">
+                            <a href="#" class="text-white">
+                                <img src="/img/portfolio.png" class="card-img" alt="Portfolio">
+                                <div class="overlay-content card-img-overlay text-bottom">
+                                    <h5>Website Development</h5>
+                                    <span>Digital Creative, Website Development</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- app/Views/home/services.php
+++ app/Views/home/services.php
@@ -6,27 +6,36 @@
     <div class="p-5">
         <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
             <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="pills-all-tab" data-bs-toggle="pill" data-bs-target="#pills-all" type="button" role="tab" aria-controls="pills-all" aria-selected="true">All</button>
+                <button class="nav-link active" id="pills-all-tab" data-bs-toggle="pill" data-bs-target="#pills-all"
+                    type="button" role="tab" aria-controls="pills-all" aria-selected="true">All</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-advertising-tab" data-bs-toggle="pill" data-bs-target="#pills-advertising" type="button" role="tab" aria-controls="pills-advertising" aria-selected="false">Advertising</button>
+                <button class="nav-link" id="pills-advertising-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-advertising" type="button" role="tab" aria-controls="pills-advertising"
+                    aria-selected="false">Advertising</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-digitalmarketing-tab" data-bs-toggle="pill" data-bs-target="#pills-digitalmarketing" type="button" role="tab" aria-controls="pills-digitalmarketing" aria-selected="false">Digital Marketing</button>
+                <button class="nav-link" id="pills-digitalmarketing-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-digitalmarketing" type="button" role="tab"
+                    aria-controls="pills-digitalmarketing" aria-selected="false">Digital Marketing</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-disabled" type="button" role="tab" aria-controls="pills-disabled" aria-selected="false" disabled>Disabled</button>
+                <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-disabled"
+                    type="button" role="tab" aria-controls="pills-disabled" aria-selected="false"
+                    disabled>Disabled</button>
             </li>
         </ul>
         <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade show active" id="pills-all" role="tabpanel" aria-labelledby="pills-all-tab" tabindex="0">
+            <div class="tab-pane fade show active" id="pills-all" role="tabpanel" aria-labelledby="pills-all-tab"
+                tabindex="0">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     <div class="col">
                         <div class="card h-100">
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Pembuatan Website</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
                                 <a href="#" class="btn btn-primary">Learn More</a>
                             </div>
                         </div>
@@ -46,7 +55,8 @@
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Produksi Neon Box</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content.</p>
                                 <a href="#" class="btn btn-primary">Learn More</a>
                             </div>
                         </div>
@@ -56,21 +66,24 @@
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Produksi Papan Reklame</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
                                 <a href="#" class="btn btn-primary">Learn More</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="tab-pane fade" id="pills-advertising" role="tabpanel" aria-labelledby="pills-advertising-tab" tabindex="0">
+            <div class="tab-pane fade" id="pills-advertising" role="tabpanel" aria-labelledby="pills-advertising-tab"
+                tabindex="0">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     <div class="col">
                         <div class="card h-100">
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Produksi Neon Box</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
                                 <a href="#" class="btn btn-primary">Learn More</a>
                             </div>
                         </div>
@@ -90,21 +103,24 @@
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Produksi Papan Reklame</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content.</p>
                                 <a href="#" class="btn btn-primary">Learn More</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="tab-pane fade" id="pills-digitalmarketing" role="tabpanel" aria-labelledby="pills-digitalmarketing-tab" tabindex="0">
+            <div class="tab-pane fade" id="pills-digitalmarketing" role="tabpanel"
+                aria-labelledby="pills-digitalmarketing-tab" tabindex="0">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     <div class="col">
                         <div class="card h-100">
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Pembuatan Website</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
                                 <a href="#" class="btn btn-primary">Learn More</a>
                             </div>
                         </div>
@@ -124,7 +140,8 @@
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content.</p>
                                 <a href="#" class="btn btn-primary">Learn More</a>
                             </div>
                         </div>
@@ -134,14 +151,16 @@
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
                                 <a href="#" class="btn btn-primary">Learn More</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="tab-pane fade" id="pills-disabled" role="tabpanel" aria-labelledby="pills-disabled-tab" tabindex="0">...
+            <div class="tab-pane fade" id="pills-disabled" role="tabpanel" aria-labelledby="pills-disabled-tab"
+                tabindex="0">...
             </div>
         </div>
     </div>
