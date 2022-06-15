d2b4fbd3ce1dd7686c614d9980379a7f07515fa5
Dzaky Fadli Firmansyah <zaaaafl654@gmail.com>
2022-06-15 12:35:39 +0700

    change

--- app/Views/home/lets-talk.php
+++ app/Views/home/lets-talk.php
@@ -1,3 +1,10 @@
 <?= $this->include('layouts/template'); ?>
 <?= $this->section('content'); ?>
+<div class="talk-content">
+    <main class="talk-main">
+        <div class="talk-title-wrap">
+
+        </div>
+    </main>
+</div>
 <?= $this->endSection(); ?>
