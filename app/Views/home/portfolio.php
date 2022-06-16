68d8266b424b674600d9d4f968c34761bfcd4468
Zion Exeon Ch <zionexeon@gmail.com>
2022-06-16 23:46:27 +0700

    add new code file

--- app/Views/home/portfolio.php
+++ app/Views/home/portfolio.php
@@ -4,34 +4,39 @@
     <div class="background-portfolio">
         <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
             <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="pills-all-tab" data-bs-toggle="pill" data-bs-target="#pills-all"
-                    type="button" role="tab" aria-controls="pills-all" aria-selected="true">All</button>
+                <button class="nav-link active button" id="pills-all-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-all" type="button" role="tab" aria-controls="pills-all"
+                    aria-selected="true">All</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-timbul-tab" data-bs-toggle="pill" data-bs-target="#pills-timbul"
-                    type="button" role="tab" aria-controls="pills-timbul" aria-selected="false">Huruf Timbul</button>
+                <button class="nav-link button" id="pills-timbul-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-timbul" type="button" role="tab" aria-controls="pills-timbul"
+                    aria-selected="false">Huruf Timbul</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-neonbox-tab" data-bs-toggle="pill" data-bs-target="#pills-neonbox"
-                    type="button" role="tab" aria-controls="pills-neonbox" aria-selected="false">Neonbox</button>
+                <button class="nav-link button" id="pills-neonbox-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-neonbox" type="button" role="tab" aria-controls="pills-neonbox"
+                    aria-selected="false">Neonbox</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-reklame-tab" data-bs-toggle="pill" data-bs-target="#pills-reklame"
-                    type="button" role="tab" aria-controls="pills-reklame" aria-selected="false">Project
+                <button class="nav-link button" id="pills-reklame-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-reklame" type="button" role="tab" aria-controls="pills-reklame"
+                    aria-selected="false">Project
                     Reklame</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-besi-tab" data-bs-toggle="pill" data-bs-target="#pills-besi"
+                <button class="nav-link button" id="pills-besi-tab" data-bs-toggle="pill" data-bs-target="#pills-besi"
                     type="button" role="tab" aria-controls="pills-besi" aria-selected="false">Pekerjaan Besi dan
                     Aluminium</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-web-tab" data-bs-toggle="pill" data-bs-target="#pills-web"
+                <button class="nav-link button" id="pills-web-tab" data-bs-toggle="pill" data-bs-target="#pills-web"
                     type="button" role="tab" aria-controls="pills-web" aria-selected="false">Web Development</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-creative-tab" data-bs-toggle="pill" data-bs-target="#pills-creative"
-                    type="button" role="tab" aria-controls="pills-creative" aria-selected="false">Digital
+                <button class="nav-link button" id="pills-creative-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-creative" type="button" role="tab" aria-controls="pills-creative"
+                    aria-selected="false">Digital
                     Creative</button>
             </li>
         </ul>
@@ -40,7 +45,7 @@
                 tabindex="0">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     <div class="col">
-                        <div class="card bg-black overlay card-hover border-0">
+                        <div class="card bg-black overlay card-hover1 border-0">
                             <a href="#" class="text-white">
                                 <img src="/img/portfolio.png" class="card-img" alt="Portfolio">
                                 <div class="overlay-content card-img-overlay text-bottom">
@@ -51,7 +56,7 @@
                         </div>
                     </div>
                     <div class="col">
-                        <div class="card bg-black overlay card-hover border-0">
+                        <div class="card bg-black overlay card-hover2 border-0">
                             <a href="#" class="text-white">
                                 <img src="/img/kanopi.jpg" class="card-img" alt="Kanopi">
                                 <div class="overlay-content card-img-overlay text-bottom">
@@ -62,7 +67,7 @@
                         </div>
                     </div>
                     <div class="col">
-                        <div class="card bg-black overlay card-hover border-0">
+                        <div class="card bg-black overlay card-hover3 border-0">
                             <a href="#" class="text-white">
                                 <img src="/img/pagarbesi.jpg" class="card-img" alt="Pagar Besi">
                                 <div class="overlay-content card-img-overlay text-bottom">
@@ -86,7 +91,7 @@
             <div class="tab-pane fade" id="pills-besi" role="tabpanel" aria-labelledby="pills-besi-tab" tabindex="0">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     <div class="col">
-                        <div class="card bg-black overlay card-hover border-0">
+                        <div class="card bg-black overlay card-hover2 border-0">
                             <a href="#" class="text-white">
                                 <img src="/img/kanopi.jpg" class="card-img" alt="Kanopi">
                                 <div class="overlay-content card-img-overlay text-bottom">
@@ -97,7 +102,7 @@
                         </div>
                     </div>
                     <div class="col">
-                        <div class="card bg-black overlay card-hover border-0">
+                        <div class="card bg-black overlay card-hover3 border-0">
                             <a href="#" class="text-white">
                                 <img src="/img/pagarbesi.jpg" class="card-img" alt="Pagar Besi">
                                 <div class="overlay-content card-img-overlay text-bottom">
@@ -112,7 +117,7 @@
             <div class="tab-pane fade" id="pills-web" role="tabpanel" aria-labelledby="pills-web-tab" tabindex="0">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     <div class="col">
-                        <div class="card bg-black overlay card-hover border-0">
+                        <div class="card bg-black overlay card-hover1 border-0">
                             <a href="#" class="text-white">
                                 <img src="/img/portfolio.png" class="card-img" alt="Portfolio">
                                 <div class="overlay-content card-img-overlay text-bottom">
@@ -128,7 +133,7 @@
                 tabindex="0">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     <div class="col">
-                        <div class="card bg-black overlay card-hover border-0">
+                        <div class="card bg-black overlay card-hover1 border-0">
                             <a href="#" class="text-white">
                                 <img src="/img/portfolio.png" class="card-img" alt="Portfolio">
                                 <div class="overlay-content card-img-overlay text-bottom">
