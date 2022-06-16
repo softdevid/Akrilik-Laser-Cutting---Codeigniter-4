086b62a4a78f0927088dbed055d372ff52dbe833
Zion Exeon Ch <zionexeon@gmail.com>
2022-06-17 06:48:44 +0700

    javascript

--- public/javascript/portfolio.js
+++ public/javascript/portfolio.js
@@ -1,21 +1,19 @@
-if ((".button" = ".active")) {
-  gsap.from(".card-hover1", {
-    duration: 1,
-    opacity: 0,
-    y: -100,
-  });
+gsap.from(".card-hover1", {
+  duration: 1,
+  opacity: 0,
+  y: -100,
+});
 
-  gsap.from(".card-hover2", {
-    duration: 1,
-    opacity: 0,
-    delay: 0.5,
-    y: -100,
-  });
+gsap.from(".card-hover2", {
+  duration: 1,
+  opacity: 0,
+  delay: 0.5,
+  y: -100,
+});
 
-  gsap.from(".card-hover3", {
-    duration: 1,
-    opacity: 0,
-    delay: 1,
-    y: -100,
-  });
-}
+gsap.from(".card-hover3", {
+  duration: 1,
+  opacity: 0,
+  delay: 1,
+  y: -100,
+});
