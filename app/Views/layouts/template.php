a236312ce42fc99339aa9350eead742cc0b41915
Ardianto <ardianto8306@gmail.com>
2022-06-16 11:44:32 +0700

    button top

--- app/Views/layouts/template.php
+++ app/Views/layouts/template.php
@@ -13,6 +13,35 @@
     <?= $this->include('layouts/navbar'); ?>
     <?= $this->renderSection('content'); ?>
     <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+    <button onclick="topFunction()" class="back-to-top" id="myBtn" title="Go to top"><i class="fa fa-chevron-up"></i></button>
+
+    <!-- scroll top button -->
+
+    <script>
+        //Get the button:
+        mybutton = document.getElementById("myBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+    </script>
 </body>
 
 </html>
