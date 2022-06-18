03850b8a2664fdc765c132b1662a860e34525add
Zion Exeon Ch <zionexeon@gmail.com>
2022-06-18 15:19:19 +0700

    code edit

--- app/Views/about/profile.php
+++ app/Views/about/profile.php
@@ -9,6 +9,52 @@
     </div>
 </div>
 <div class="container">
-
+    <div class="row profile-p">
+        <article>
+            <p> <b>Mekar Laser Cutting Digital</b> adalah perusahaan Advertising dan Digital Marketing yang berbasis di
+                kota Purbalingga.
+                <b>Mekar Laser Cutting Digital</b> sudah berpengalaman dan memiliki ratusan portfolio dalam bidang
+                periklanan, promosi dan branding
+                selama lebih dari 5 tahun. Spesialis Jasa Pembuatan Reklame Huruf Timbul, Neon Box, Baliho dan Branding
+                instansi yang selalu mengutamakan
+                kualitas dan mengedepankan professionalisme kerja serta berkomitmen memberikan harga yang realistis bagi
+                klien. Selain produksi Reklame, <b>Mekar Laser Cutting Digital</b> juga melayani kebutuhan Digital
+                Marketing sebagai solusi pemasaran digital. Kami menawarkan beberapa macam kebutuhan akan jasa digital
+                marketing antara lain, pembuatan website, optimasi website, audit dan analisa website / social media,
+                pengelolaan social media, foto produk, pembuatan logo, personal / politik branding.
+            </p>
+        </article>
+        <article>
+            <p>
+            <h3 class="fw-bold">Dukungan Mesin dan Tenaga Ahli</h3>
+            </p>
+            <p>Perusahaan kami didukung oleh mesin cutting laser dan mesin bending dengan teknologi modern, tenaga ahli
+                yang berpengalaman dan material yang berkualitas yang menjadikan kami unggul dalam segi harga, kualitas
+                dan kuantitas produksi.</p>
+        </article>
+        <article>
+            <p>
+            <h3 class="fw-bold">Service Area</h3>
+            </p>
+            <p>Kami melayani order pengerjaan huruf timbul maupun cutting laser dan segala bentuk kebutuhan produk
+                selain reklame. Area service kami adalah seluruh daerah di Negara Kesatuan Republik Indonesia, khususnya
+                daerah Jawa Tengah, Banyumas, Purwokerto, Purbalingga, Cilacap, Banjarnegara, Wonosobo, Bumiayu, Tegal,
+                Pekalongan, Bandung, Sukabumi, Kebumen, Purworejo, Kutoarjo, Semarang, Kudus, Pati, Kendal, Sukoharjo,
+                Solo, Klaten, Yogyakarta dan daerah lainnya.</p>
+        </article>
+        <article>
+            <p>
+            <h3 class="fw-bold">Visi Misi Alegra Advertising</h3>
+            </p>
+            <ul>
+                <li>Mencari Ridho Allah azza wa Jalla</li>
+                <li>Menghasilkan produk dan jasa dengan kecepatan dan mutu berstandar tinggi</li>
+                <li>Mensejahterakan personil perusahaan dan keluarganya, serta memberikan kebermanfaatan bagi lingkungan
+                </li>
+                <li>Layanan yang terpuji dan berkesan</li>
+                <li>Sistem operasi yang rapi dan jelas</li>
+            </ul>
+        </article>
+    </div>
 </div>
 <?= $this->endSection(); ?>
