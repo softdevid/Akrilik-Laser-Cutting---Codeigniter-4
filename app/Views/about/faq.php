2c438f3a79df265b1bfdaeaccf46157d9ff02a4c
Zion Exeon Ch <zionexeon@gmail.com>
2022-06-19 14:40:25 +0700

    add code

--- app/Views/about/faq.php
+++ app/Views/about/faq.php
@@ -1,3 +1,130 @@
 <?= $this->extend('layouts/template'); ?>
 <?= $this->section('content'); ?>
+<div class="jumbotron bg-cover text-white"
+    style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.6) 0%,rgba(0,0,0,0.6) 100%), url(/img/about/faq.png); padding: 120px; width: 100%;">
+    <div class="container">
+        <h1 class="display-7">FAQ : Answer Question</h1>
+        <!-- <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+            featured content or information.</p> -->
+    </div>
+</div>
+<div class="container">
+    <div class="row profile-p">
+        <div class="col-md-8">
+            <article>
+                <p>
+                <h3 class="fw-bold">Pertanyaan Seputar Advertising Reklame</h3>
+                <div class="accordion pt-3" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Apakah Mekar Laser Cutting Digital dapat dipercaya?
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                Tentu saja kami dapat dipercaya, Insya Allah kami amanah. Kami tidak akan menyia-nyiakan
+                                reputasi kami selama 5 tahun sebagai perusahaan Advertising yang berkembang di
+                                Purwokerto. Alegra Advertising juga memiliki legalitas dan izin usaha bidang Advertising
+                                dan Periklanan yang tercatat di Kementerian Hukum dan HAM atas nama CV. Alegra Multi
+                                Mandiri.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Dimana alamat kantor dan workshop Mekar Laser Cutting Digital?
+                            </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                JL. Lingkar Purbalingga
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                Hal apa saja yang dikerjakan?
+                            </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                Kami biasa mengerjakan reklame pembuatan huruf timbul, neon box, pylon sign, neon sign,
+                                dll. Selain itu kami juga biasa mengerjakan pembuatan dan pemasangan booth pameran,
+                                backdrop, kanopi, pagar, atap baja ringan. Kami juga menerima pembuatan gerobak untuk
+                                jualan.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article>
+                <p>
+                <h3 class="fw-bold pt-3">Pertanyaan Seputar Digital Creative</h3>
+                <div class="accordion pt-3" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFour">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+                                Apa itu Digital Creative Agency?
+                            </button>
+                        </h2>
+                        <div id="collapseFour" class="accordion-collapse collapse show" aria-labelledby="headingFour"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                Pengertian digital creative agency adalah agen baik perorangan atau perusahaan sebagai
+                                pihak ketiga yang membantu klien untuk membuat, merancang, dan mengelola kegiatan
+                                pemasaran digital.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFive">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                Apa aja sih yang bisa Alegra Creative lakukan?
+                            </button>
+                        </h2>
+                        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                Kami sebagai perusahaan Advertising yang bergerak dalam industri kreatif pada dasarnya
+                                bisa melakukan apapun, mulai dari Pembuatan Branding Logo Usaha, Pembuatan dan
+                                Pengelolaan Website (Website Development) dan Sosial Media Management (SMM), Search
+                                Engine Optimization (SEO), Foto Produk Profesional, Content dan Media Placement,
+                                Pembuatan Content Marketing, Analisa serta Audit Sosial Media dan Website
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingSix">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                                Siapa saja yang butuh Jasa Digital Marketing?
+                            </button>
+                        </h2>
+                        <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                Mulai dari pengusaha, pebisnis, perusahaan, umkm, ukm, startup & siapa saja yang ingin
+                                memasarkan produk/brand nya secara online. Mulai dari yang ingin mendapatkan trafik &
+                                mendapatkan leads dari digital marketing, branding hingga mengoptimalkan penjualan suatu
+                                produk.Tentunya harus diimbangi dengan keyakinan dan kerja keras.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+</div>
 <?= $this->endSection(); ?>
