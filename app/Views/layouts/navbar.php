bb773d72965b3cec466aad7a39ab38579788036e
Ardianto <ardianto8306@gmail.com>
2022-06-20 12:58:08 +0700

    uri

--- app/Views/layouts/navbar.php
+++ app/Views/layouts/navbar.php
@@ -9,8 +9,7 @@
 
         <ul class="nav d-flex justify-content-end align-items-center">
             <li class="nav-item">
-                <a class="nav-link text-white active" aria-current="page" href="#"><i
-                        class="fa-brands fa-youtube"></i></a>
+                <a class="nav-link text-white active" aria-current="page" href="#"><i class="fa-brands fa-youtube"></i></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link text-white" href="#"><i class="fa-brands fa-facebook"></i></a>
@@ -31,18 +30,16 @@
         <a class="navbar-brand" href="#">MEKAR LASER <br>
             <span class="badge bg-dark text-white">Cutting Digital</span>
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    <a class="nav-link" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         About
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
