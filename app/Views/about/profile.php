55bf7b3ae520ac107b820b340193fb8d9817e696
Ardianto <ardianto8306@gmail.com>
2022-06-21 12:08:58 +0700

    profil

--- app/Views/about/profile.php
+++ app/Views/about/profile.php
@@ -8,104 +8,111 @@
     </div>
 </div>
 <div class="container">
-    <div class="row profile-p">
-        <article class="article-1">
-            <p> <b>Mekar Laser Cutting Digital</b> adalah perusahaan Advertising dan Digital Marketing yang berbasis di
-                kota Purbalingga.
-                <b>Mekar Laser Cutting Digital</b> sudah berpengalaman dan memiliki ratusan portfolio dalam bidang
-                periklanan, promosi dan branding
-                selama lebih dari 5 tahun. Spesialis Jasa Pembuatan Reklame Huruf Timbul, Neon Box, Baliho dan Branding
-                instansi yang selalu mengutamakan
-                kualitas dan mengedepankan professionalisme kerja serta berkomitmen memberikan harga yang realistis bagi
-                klien. Selain produksi Reklame, <b>Mekar Laser Cutting Digital</b> juga melayani kebutuhan Digital
-                Marketing sebagai solusi pemasaran digital. Kami menawarkan beberapa macam kebutuhan akan jasa digital
-                marketing antara lain, pembuatan website, optimasi website, audit dan analisa website / social media,
-                pengelolaan social media, foto produk, pembuatan logo, personal / politik branding.
-            </p>
-        </article>
-        <article class="article-2">
-            <p>
-            <h3 class="fw-bold">Dukungan Mesin dan Tenaga Ahli</h3>
-            </p>
-            <p>Perusahaan kami didukung oleh mesin cutting laser dan mesin bending dengan teknologi modern, tenaga ahli
-                yang berpengalaman dan material yang berkualitas yang menjadikan kami unggul dalam segi harga, kualitas
-                dan kuantitas produksi.</p>
-        </article>
-        <article class="article-3">
-            <p>
-            <h3 class="fw-bold">Service Area</h3>
-            </p>
-            <p>Kami melayani order pengerjaan huruf timbul maupun cutting laser dan segala bentuk kebutuhan produk
-                selain reklame. Area service kami adalah seluruh daerah di Negara Kesatuan Republik Indonesia, khususnya
-                daerah Jawa Tengah, Banyumas, Purwokerto, Purbalingga, Cilacap, Banjarnegara, Wonosobo, Bumiayu, Tegal,
-                Pekalongan, Bandung, Sukabumi, Kebumen, Purworejo, Kutoarjo, Semarang, Kudus, Pati, Kendal, Sukoharjo,
-                Solo, Klaten, Yogyakarta dan daerah lainnya.</p>
-        </article>
-        <article class="article-4">
-            <p>
-            <h3 class="fw-bold">Visi Misi Alegra Advertising</h3>
-            </p>
-            <ul>
-                <li>Mencari Ridho Allah azza wa Jalla</li>
-                <li>Menghasilkan produk dan jasa dengan kecepatan dan mutu berstandar tinggi</li>
-                <li>Mensejahterakan personil perusahaan dan keluarganya, serta memberikan kebermanfaatan bagi lingkungan
-                </li>
-                <li>Layanan yang terpuji dan berkesan</li>
-                <li>Sistem operasi yang rapi dan jelas</li>
-            </ul>
-        </article>
-        <article class="article-5 p-3">
-            <p>
-            <h3 class="fw-bold">Aktivitas Desain Komunikasi Visual/ Desain Grafis</h3>
-            </p>
-            <p>Kelompok ini mencakup kegiatan penyediaan jasa desain komunikasi visual/desain grafis secara manual
-                maupun digital, serta statis (tidak bergerak) maupun dinamis (bergerak, interaktif), pada media cetak,
-                layar (gawai, tv, komputer, layar LED dan sejenisnya), luring, daring atau virtual, yang berhubungan
-                dengan pembuatan materi dengan fungsi identifikasi, informasi dan persuasi yang diimplementasikan pada
-                identitas jenama (brand), logo, desain iklan, infografik, dan stasioneri.</p>
-        </article>
-        <article class="article-6 p-3">
-            <p>
-            <h3 class="fw-bold">Aktivitas Konsultasi dan Perancangan Internet of Things (IoT)</h3>
-            </p>
-            <p>Kelompok ini mencakup kegiatan layanan konsultasi, perancangan dan pembuatan solusi sistem terintegrasi
-                berdasarkan pesanan (bukan siap pakai) dengan cara memodifikasi perangkat keras (hardware) yang sudah
-                ada, seperti sensor, microcontroller, dan perangkat keras (hardware) lainnya. Modifikasi tersebut
-                dilakukan pada perangkat keras (hardware) IoT dan/atau perangkat lunak (software) yang tertanam
-                didalamnya. Kelompok ini tidak mencakup aktivitas manufaktur chip (26120) dan aktivitas
-                penerbitan/pengembangan perangkat lunak IoT (58200 dan 62019).</p>
-        </article>
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            <div class="col" data-aos="fade-right" data-aos-duration="1000">
-                <div class="h-100 border-0">
-                    <img src="/img/profil/iot1.jpeg" class="card-img-top" id="img-profil" alt="...">
+    <!-- <div class="profile-p"> -->
+    <article class="article-1">
+        <p> <b>Mekar Laser Cutting Digital</b> adalah perusahaan Advertising dan Digital Marketing yang berbasis di
+            kota Purbalingga.
+            <b>Mekar Laser Cutting Digital</b> sudah berpengalaman dan memiliki ratusan portfolio dalam bidang
+            periklanan, promosi dan branding
+            selama lebih dari 5 tahun. Spesialis Jasa Pembuatan Reklame Huruf Timbul, Neon Box, Baliho dan Branding
+            instansi yang selalu mengutamakan
+            kualitas dan mengedepankan professionalisme kerja serta berkomitmen memberikan harga yang realistis bagi
+            klien. Selain produksi Reklame, <b>Mekar Laser Cutting Digital</b> juga melayani kebutuhan Digital
+            Marketing sebagai solusi pemasaran digital. Kami menawarkan beberapa macam kebutuhan akan jasa digital
+            marketing antara lain, pembuatan website, optimasi website, audit dan analisa website / social media,
+            pengelolaan social media, foto produk, pembuatan logo, personal / politik branding.
+        </p>
+    </article>
+    <article class="article-2">
+        <p>
+        <h3 class="fw-bold">Dukungan Mesin dan Tenaga Ahli</h3>
+        </p>
+        <p>Perusahaan kami didukung oleh mesin cutting laser dan mesin bending dengan teknologi modern, tenaga ahli
+            yang berpengalaman dan material yang berkualitas yang menjadikan kami unggul dalam segi harga, kualitas
+            dan kuantitas produksi.</p>
+    </article>
+    <article class="article-3">
+        <p>
+        <h3 class="fw-bold">Service Area</h3>
+        </p>
+        <p>Kami melayani order pengerjaan huruf timbul maupun cutting laser dan segala bentuk kebutuhan produk
+            selain reklame. Area service kami adalah seluruh daerah di Negara Kesatuan Republik Indonesia, khususnya
+            daerah Jawa Tengah, Banyumas, Purwokerto, Purbalingga, Cilacap, Banjarnegara, Wonosobo, Bumiayu, Tegal,
+            Pekalongan, Bandung, Sukabumi, Kebumen, Purworejo, Kutoarjo, Semarang, Kudus, Pati, Kendal, Sukoharjo,
+            Solo, Klaten, Yogyakarta dan daerah lainnya.</p>
+    </article>
+    <article class="article-4">
+        <p>
+        <h3 class="fw-bold">Visi Misi Alegra Advertising</h3>
+        </p>
+        <ul>
+            <li>Mencari Ridho Allah azza wa Jalla</li>
+            <li>Menghasilkan produk dan jasa dengan kecepatan dan mutu berstandar tinggi</li>
+            <li>Mensejahterakan personil perusahaan dan keluarganya, serta memberikan kebermanfaatan bagi lingkungan
+            </li>
+            <li>Layanan yang terpuji dan berkesan</li>
+            <li>Sistem operasi yang rapi dan jelas</li>
+        </ul>
+    </article>
+    <article class="article-5 pt-5">
+        <p>
+        <h3 class="fw-bold">Aktivitas Desain Komunikasi Visual/ Desain Grafis</h3>
+        </p>
+        <p>Kelompok ini mencakup kegiatan penyediaan jasa desain komunikasi visual/desain grafis secara manual
+            maupun digital, serta statis (tidak bergerak) maupun dinamis (bergerak, interaktif), pada media cetak,
+            layar (gawai, tv, komputer, layar LED dan sejenisnya), luring, daring atau virtual, yang berhubungan
+            dengan pembuatan materi dengan fungsi identifikasi, informasi dan persuasi yang diimplementasikan pada
+            identitas jenama (brand), logo, desain iklan, infografik, dan stasioneri.</p>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col" data-aos="fade-right" data-aos-duration="1000">
+                    <div class="h-100 border-0">
+                        <img src="/img/profil/desain1.jpeg" class="card-img-top" id="img-profil-desain" alt="...">
+                    </div>
                 </div>
-            </div>
-            <div class="col" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="100">
-                <div class="h-100 border-0">
-                    <img src="/img/profil/iot2.jpeg" class="card-img-top" id="img-profil" alt="...">
+                <div class="col" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="100">
+                    <div class="h-100 border-0">
+                        <img src="/img/profil/desain2.png" class="card-img-top" id="img-profil-desain" alt="...">
+                    </div>
                 </div>
-            </div>
-            <div class="col" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="250">
-                <div class="h-100 border-0">
-                    <img src="/img/profil/iot3.jpeg" class="card-img-top" id="img-profil" alt="...">
+                <div class="col" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="250">
+                    <div class="h-100 border-0">
+                        <img src="/img/profil/desain3.jpeg" class="card-img-top" id="img-profil-desain" alt="...">
+                    </div>
                 </div>
             </div>
         </div>
-
-        <article class="article-7 p-3">
-            <p>
-            <h3 class="fw-bold">Industri Pengolahan Lainnya YTDL</h3>
-            </p>
-            <p>Kelompok ini mencakup usaha pembuatan barang-barang yang tidak diklasifikasikan di tempat lain, seperti
-                papan nama, papan reklame (papan atau lampu display), segala macam payung, pipa rokok, lencana, piala,
-                medali, stempel, tongkat, kap lampu, lilin, segala macam kancing, sapu, sikat ijuk, tempat cerutu dan
-                sirih, pipa rokok, sisir, penyemprot wangi-wangian, botol vakum dan bejana vakum untuk keperluan pribadi
-                atau rumah tangga, wig (rambut palsu), janggut palsu, bulu mata palsu dan lainnya yang belum tercakup
-                dalam golongan lainnya, termasuk pembuatan korek api dari logam dan pembuatan perhiasan imitasi, boneka
-                yang digunakan oleh penjahit untuk mengenakan baju, peti jenazah. Pembuatan sumbu lampu dimasukan dalam
-                kelompok 13942.</p>
-        </article>
-    </div>
+    </article>
+    <article class="article-6 pt-5">
+        <p>
+        <h3 class="fw-bold">Aktivitas Konsultasi dan Perancangan Internet of Things (IoT)</h3>
+        </p>
+        <p>Kelompok ini mencakup kegiatan layanan konsultasi, perancangan dan pembuatan solusi sistem terintegrasi
+            berdasarkan pesanan (bukan siap pakai) dengan cara memodifikasi perangkat keras (hardware) yang sudah
+            ada, seperti sensor, microcontroller, dan perangkat keras (hardware) lainnya. Modifikasi tersebut
+            dilakukan pada perangkat keras (hardware) IoT dan/atau perangkat lunak (software) yang tertanam
+            didalamnya. Kelompok ini tidak mencakup aktivitas manufaktur chip (26120) dan aktivitas
+            penerbitan/pengembangan perangkat lunak IoT (58200 dan 62019).</p>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col" data-aos="fade-right" data-aos-duration="1000">
+                    <div class="h-100 border-0">
+                        <img src="/img/profil/iot1.jpeg" class="card-img-top" id="img-profil" alt="...">
+                    </div>
+                </div>
+                <div class="col" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="100">
+                    <div class="h-100 border-0">
+                        <img src="/img/profil/iot2.jpeg" class="card-img-top" id="img-profil" alt="...">
+                    </div>
+                </div>
+                <div class="col" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="250">
+                    <div class="h-100 border-0">
+                        <img src="/img/profil/iot3.jpeg" class="card-img-top" id="img-profil" alt="...">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+    <!-- </div> -->
 </div>
 <?= $this->endSection(); ?>
