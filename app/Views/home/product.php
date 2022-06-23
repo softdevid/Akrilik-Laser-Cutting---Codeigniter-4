27acd0a3a866f2e641728fe9078af1f85e1f4061
Zion Exeon Ch <zionexeon@gmail.com>
2022-06-23 10:53:08 +0700

    add code

--- app/Views/home/product.php
+++ app/Views/home/product.php
@@ -11,7 +11,7 @@
 <div class="container">
     <div class="row isi-header">
         <div class="col-md-6">
-            <img src="/img/product/GP.png" class="header-produk" alt="GP">
+            <img src="/img/product/GP.png" class="header-produk pb-4" alt="GP">
         </div>
         <div class="col-md-6">
             <article>
@@ -47,6 +47,83 @@
     </div>
 </div>
 <div class="container">
-    <div class="row"></div>
+    <div class="row">
+        <h5 class="pb-4">Varian Produk</h5>
+        <div class="justify-content-center">
+            <div class="col-md-12">
+                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active button" id="pills-all-tab" data-bs-toggle="pill"
+                            data-bs-target="#pills-all" type="button" role="tab" aria-controls="pills-all"
+                            aria-selected="true">All</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link button" id="pills-timbul-tab" data-bs-toggle="pill"
+                            data-bs-target="#pills-timbul" type="button" role="tab" aria-controls="pills-timbul"
+                            aria-selected="false">Huruf Timbul</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link button" id="pills-neonbox-tab" data-bs-toggle="pill"
+                            data-bs-target="#pills-neonbox" type="button" role="tab" aria-controls="pills-neonbox"
+                            aria-selected="false">Neonbox</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link button" id="pills-reklame-tab" data-bs-toggle="pill"
+                            data-bs-target="#pills-reklame" type="button" role="tab" aria-controls="pills-reklame"
+                            aria-selected="false">Project
+                            Reklame</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link button" id="pills-digitalcreative-tab" data-bs-toggle="pill"
+                            data-bs-target="#pills-digitalcreative" type="button" role="tab"
+                            aria-controls="pills-digitalcreative" aria-selected="false">Digital Creative</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link button" id="pills-iot-tab" data-bs-toggle="pill"
+                            data-bs-target="#pills-iot" type="button" role="tab" aria-controls="pills-iot"
+                            aria-selected="false">Prototype IOT</button>
+                    </li>
+                </ul>
+                <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade show active" id="pills-all" role="tabpanel"
+                        aria-labelledby="pills-all-tab" tabindex="0">
+                        <div class="row row-cols-1 row-cols-md-3 g-4">
+
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="pills-timbul" role="tabpanel" aria-labelledby="pills-timbul-tab"
+                        tabindex="0">
+                        <div class="row row-cols-1 row-cols-md-3 g-4">
+
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="pills-neonbox" role="tabpanel" aria-labelledby="pills-neonbox-tab"
+                        tabindex="0">
+                        <div class="row row-cols-1 row-cols-md-3 g-4">
+
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="pills-reklame" role="tabpanel" aria-labelledby="pills-reklame-tab"
+                        tabindex="0">
+                        <div class="row row-cols-1 row-cols-md-3 g-4">
+
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="pills-digitalcreative" role="tabpanel"
+                        aria-labelledby="pills-digitalcreative-tab" tabindex="0">
+                        <div class="row row-cols-1 row-cols-md-3 g-4">
+
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="pills-iot" role="tabpanel" aria-labelledby="pills-iot-tab"
+                        tabindex="0">
+                        <div class="row row-cols-1 row-cols-md-3 g-4">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <?= $this->endSection(); ?>
